33a37e27a54b597ddae14d25e41c738631754064
Vignesh <96247274+vignesh-wikki@users.noreply.github.com>
2023-07-28 23:17:20 +0530

    Update BookForm.js

--- src/Components/BookForm.js
+++ src/Components/BookForm.js
@@ -30,14 +30,14 @@ movieName
     <form className="m-4  max-w-lg" onSubmit={handleSubmit}>
   <div className="flex flex-wrap -mx-3 mb-6">
     <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+      <label className="block uppercase tracking-wide text-pink-900 text-xs font-bold mb-2" >
         Movie name
       </label>
       <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"  type="text" value={data.data} />
      
     </div>
     <div className="w-full md:w-1/2 px-3">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+      <label className="block uppercase tracking-wide text-pink-900 text-xs font-bold mb-2" >
         Name
       </label>
       <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="enter your name" onChange={(e)=>{
@@ -47,10 +47,10 @@ movieName
   </div>
   <div class="flex flex-wrap -mx-3 mb-6">
     <div class="w-full px-3">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+      <label class="block uppercase tracking-wide text-pink-900 text-xs font-bold mb-2" >
     Phone number
       </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="number"
+      <input className="appearance-none block w-full bg-gray-200 text-pink-900 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="number"
         onChange={(e)=>{
           setNumber(e.target.value)
         }}
@@ -60,24 +60,24 @@ movieName
   </div>
   <div className="flex flex-wrap -mx-3 mb-2">
     <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+      <label className="block uppercase tracking-wide text-pink-900 text-xs font-bold mb-2" >
         City
       </label>
-      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="Albuquerque" onChange={(e)=>{
+      <input className="appearance-none block w-full bg-gray-200 text-pink-900 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="Albuquerque" onChange={(e)=>{
         setCity(e.target.value)
       }} required/>
     </div>
     
   
     <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+      <label className="block uppercase tracking-wide text-pink-900 text-xs font-bold mb-2">
         Your Id
       </label>
       <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="number" value={data.id} />
     </div>
   </div>
   <div className='flex justify-center'>
-  <button className='p-2 rounded-t-2xl border-solid bg-purple-200' type='submit' onClick={()=> handleSubmit}>{button}</button>
+  <button className='hover:text-white hover:bg-pink-900 mt-6 p-2 rounded-t-2xl border-solid bg-purple-200' type='submit' onClick={()=> handleSubmit}>{button}</button>
 </div></form>
     </div>
   )
